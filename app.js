9ffec7f4c3b542501df100028b5ee7dcb7af46e0
ThomassMathisen <thomasmathisen@hotmail.com>
2022-10-25 16:08:34 +0200

    Setup of dotenv

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+require('dotenv').config();
+const bcrypt = require("bcryptjs"); 
 
-const mongoDb = "mongodb+srv://username:username123@cluster1.toawovy.mongodb.net/?retryWrites=true&w=majority";
+
+const mongoDb = process.env.MONGO_URI
 mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "mongo connection error"));
@@ -23,13 +26,50 @@ const app = express();
 app.set("views", __dirname);
 app.set("view engine", "ejs");
 
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) { 
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    });
+  })
+);
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.render("index"));
+app.get("/", (req, res) => {
+  res.render("index", { user: req.user });
+});
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
+app.get("/log-out", (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
 
 app.post("/sign-up", (req, res, next) => {
     const user = new User({
@@ -42,5 +82,13 @@ app.post("/sign-up", (req, res, next) => {
       res.redirect("/");
     });
   });
+
+  app.post(
+    "/log-in",
+    passport.authenticate("local", {
+      successRedirect: "/",
+      failureRedirect: "/"
+    })
+  );
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
